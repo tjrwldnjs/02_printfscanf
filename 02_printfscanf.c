2a616fb29cd52843f2039e7170236d9f1be789c0
tjrwldnjs <sue06024@naver.com>
2018-09-20 16:29:16 +0900

    02 exercise

--- 02_printfscanf.c
+++ 02_printfscanf.c
@@ -3,17 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int inInt;
-	float inFloat;
+int main(int argc, char *argv[]) 
+{
+	char c;
+	int i;
 	
-	printf("input an integer : ");
-	scanf("%d", &inInt);
+	printf("input a number :");
+	scanf("%c", &c);
 	
-	printf("input an float : " );
-	scanf("%f", &inFloat);
-	
-	printf("integer : %i, float : %f\n", inInt, inFloat);
+	i = c - '0' ;
+	printf("The input number is %i\n",i);
 	
 	return 0;
+
 }
